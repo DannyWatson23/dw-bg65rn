2a47c0e81ac90601ac9d70f9b3f49ef06c62ada5
DannyWatson23 <DannyWatson23@outlook.com>
2016-02-28 23:52:41 +0000

    Update installme.sh

--- installme.sh
+++ installme.sh
@@ -2,9 +2,5 @@
 echo I am installing...
 sudo apt-get update -y
 sudo apt-get install -y nginx
-box1.vm.hostname = "DannyBox1"
-box1.vm.network "private_network", type: "dhcp"
-box1.vm.network "forwarded_port", guest: 80, host: 8080, auto_correct: true
-box1.vm.network "public_network", type: "dhcp"
 echo done...
 
