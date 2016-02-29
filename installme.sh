e09337ecd53166370a54ff0a4e25af9eb81b1a7b
DannyWatson23 <DannyWatson23@outlook.com>
2016-02-29 01:39:00 +0000

    Update installme.sh

--- installme.sh
+++ installme.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
+#!/bin/sh -e
 echo I am installing...
-try {
 sudo apt-get update -y
 sudo apt-get install -y nginx
 curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
@@ -11,9 +10,5 @@ sudo yo hubot
 export HUBOT_CAMPFIRE_TOKEN="a99f3048fb245a8e639ed414e99ce9996aa11c04"
 export HUBOT_CAMPFIRE_ROOMS="620623"
 export HUBOT_CAMPFIRE_ACCOUNT="university161"
-}
-catch {
-  echo there has been an error
-}
 echo done...
 
