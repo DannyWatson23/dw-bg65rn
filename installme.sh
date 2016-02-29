a86f3b8ee207f952ca409f83b0e47970dbbeab91
DannyWatson23 <DannyWatson23@outlook.com>
2016-02-29 01:09:11 +0000

    Update installme.sh

--- installme.sh
+++ installme.sh
@@ -4,10 +4,11 @@ sudo apt-get update -y
 sudo apt-get install -y nginx
 curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
 sudo apt-get install -y nodejs
+sudo apt-get install -y build-essential
 sudo npm install -g yo generator-hubot
 mkdir Hubot
 cd Hubot
-yo hubot
+sudo yo hubot --defaults
 export HUBOT_CAMPFIRE_TOKEN="a99f3048fb245a8e639ed414e99ce9996aa11c04"
 export HUBOT_CAMPFIRE_ROOMS="620623"
 export HUBOT_CAMPFIRE_ACCOUNT="university161"
