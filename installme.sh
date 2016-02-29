615567776dd27d5041f1efaab3223dd7a2e8b4aa
DannyWatson23 <DannyWatson23@outlook.com>
2016-02-29 01:34:44 +0000

    Update installme.sh

--- installme.sh
+++ installme.sh
@@ -7,7 +7,7 @@ curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y build-essential
 sudo npm install -g yo generator-hubot
-sudo yo hubot --owner="Danny" --name="Tron" --description="I fight for the users!" --adapter=campfire
+sudo yo hubot 
 export HUBOT_CAMPFIRE_TOKEN="a99f3048fb245a8e639ed414e99ce9996aa11c04"
 export HUBOT_CAMPFIRE_ROOMS="620623"
 export HUBOT_CAMPFIRE_ACCOUNT="university161"
