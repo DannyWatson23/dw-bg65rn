3e28f3552bac4c68a2225d2b5341a1041992557b
DannyWatson23 <DannyWatson23@outlook.com>
2016-03-01 02:20:04 +0000

    Update installme.sh

--- installme.sh
+++ installme.sh
@@ -7,8 +7,5 @@ sudo apt-get install -y nodejs
 sudo apt-get install -y build-essential
 sudo npm install -g yo generator-hubot
 sudo yo hubot 
-export HUBOT_CAMPFIRE_TOKEN="a99f3048fb245a8e639ed414e99ce9996aa11c04"
-export HUBOT_CAMPFIRE_ROOMS="620623"
-export HUBOT_CAMPFIRE_ACCOUNT="university161"
 echo done...
 
