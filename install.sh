3d9723036d609e6058a3a85706ad34be24c15f15
DannyWatson23 <DannyWatson23@outlook.com>
2016-03-03 01:16:37 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/sh -e
+echo "installing curl"
 curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash - 
+echo "installing node js"
 sudo apt-get install -y nodejs
+echo "creatting hubot"
 sudo mkdir /hubot && cd /hubot
+echo "creating npm installer"
 sudo npm install -g yo generator-hubot
+echo "creating hubot with yo"
 sudo yo hubot
