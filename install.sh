3841fddf4ab7875a10e012009a0f22070a0406b8
DannyWatson23 <DannyWatson23@outlook.com>
2016-03-03 00:11:41 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
 sudo apt-get install -y nodejs
-sudo -rwx------ npm install -g yo generator-hubot
+sudo npm install -g yo generator-hubot
 sudo yo hubot
 sudo nano hubot
