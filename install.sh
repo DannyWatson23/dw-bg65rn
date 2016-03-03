06b6e2a067b14f1fa55f55b501a250405497d744
DannyWatson23 <DannyWatson23@outlook.com>
2016-03-03 17:37:10 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,4 +12,4 @@ sudo chmod -R 777 /usr/lib/node_modules/yo/node_modules/configstore
 echo "going into shared folder"
 cd /Desktop/Shared
 echo "creating hubot from shared folder"
-su yo hubot
+su vagrant yo hubot
