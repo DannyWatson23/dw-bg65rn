76f2aa02877aac1656ba4a81d54c981e78d6177a
DannyWatson23 <DannyWatson23@outlook.com>
2016-03-03 21:04:30 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo "installing curl"
-curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash - 
+curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash - -> /dev/null
 echo "installing node js"
-sudo apt-get install -y nodejs
+sudo apt-get install -y nodejs -> /dev/null
 echo "creating npm installer"
 npm install -g yo generator-hubot
 echo "going into shared folder"
