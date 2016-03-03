8040abb5246c295a2ee39b8b18364c3955a0cd08
DannyWatson23 <DannyWatson23@outlook.com>
2016-03-03 21:28:15 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,12 +4,9 @@ curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash - > /dev/null
 echo "installing node js"
 sudo apt-get install -y nodejs > /dev/null
 echo "creating npm installer"
-npm install -g yo generator-hubot
+sudo npm install -g yo generator-hubot
+sudo npm install -g yo > /dev/null
 echo "going into shared folder"
-cd /Desktop/Shared
-mkdir hubot-sample
-cd hubot-sample
-echo "creating hubot"
+sudo su
+cd /home/vagrant
 yo hubot --defaults
-echo "creating hubot from shared folder"
-sudo yo hubot -g
