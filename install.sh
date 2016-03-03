29d158976d7616b4cba8b52d730c4a5937b15cb6
DannyWatson23 <DannyWatson23@outlook.com>
2016-03-03 17:32:54 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
 echo "installing node js"
 sudo apt-get install -y nodejs
 echo "creating npm installer"
-sudo npm install -g yo generator-hubot | sudo -E bash -
+sudo npm install -g yo generator-hubot
 echo "setting permissions"
 sudo chmod -R 777 /usr/lib/node_modules/yo/node_modules/configstore
 #echo "going into sudo su root"
