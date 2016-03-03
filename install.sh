595c05fa358b621ab5d25eb7cf7f66046a3b1472
DannyWatson23 <DannyWatson23@outlook.com>
2016-03-03 21:02:59 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,12 +4,7 @@ curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
 echo "installing node js"
 sudo apt-get install -y nodejs
 echo "creating npm installer"
-sudo npm install -g yo generator-hubot
-echo "one!"
-mkdir /root/.config/configstore
-echo "two"
-chmod g+rwx /root /root/.config /root/.config/configstore
-yo -v
+npm install -g yo generator-hubot
 echo "going into shared folder"
 cd /Desktop/Shared
 mkdir hubot-sample
