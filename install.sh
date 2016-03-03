e313a16da731511d6f08748ac6f16d198aec5790
DannyWatson23 <DannyWatson23@outlook.com>
2016-03-03 18:23:20 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@
 echo "installing curl"
 curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash - 
 echo "installing node js"
-sudo apt-get install -g -y nodejs
+sudo apt-get install -y nodejs
 echo "creating npm installer"
 sudo npm install -g yo generator-hubot
 echo "setting permissions"
 sudo chmod -R 777 /usr/lib/node_modules/yo/node_modules/configstore
-sudo chmod -R 777 $vagrant /usr/lib/node_modules/yo/node_modules
-chmod g+rwx /root/.config/configstore
+sudo chmod -R 777 vagrant /usr/lib/node_modules/yo/node_modules
+chmod g+rwx -R /usr/lib/node_modules
 #echo "going into sudo su root"
 #su vagrant
 echo "going into shared folder"
