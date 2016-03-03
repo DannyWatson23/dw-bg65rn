c43b3d0e36ee9cf81101790ff342b1e1ad2f9d50
DannyWatson23 <DannyWatson23@outlook.com>
2016-03-03 16:58:17 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,8 +7,7 @@ echo "creating npm installer"
 sudo npm install -g yo generator-hubot
 echo "setting permissions"
 sudo chmod -R 777 /usr/lib/node_modules/yo/node_modules/configstore
-sudo chown -R vagrant:vagrant /root/.config/configstore
-Sudo chown vagrant /Users/vagrant/root/.config/configstore/insight-yo.json
+sudo chown vagrant /Users/vagrant/root/.config/configstore/insight-yo.json
 echo "going into sudo su root"
 su vagrant
 echo "going into shared folder"
