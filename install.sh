755c4804149856e39f2360c1d76179ce2c668e35
DannyWatson23 <DannyWatson23@outlook.com>
2016-03-03 17:08:26 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@ echo "creating npm installer"
 sudo npm install -g yo generator-hubot
 echo "setting permissions"
 sudo chmod -R 777 /usr/lib/node_modules/yo/node_modules/configstore
-echo "going into sudo su root"
-su vagrant
+#echo "going into sudo su root"
+#su vagrant
 echo "going into shared folder"
 cd /Desktop/Shared
 echo "creating hubot from shared folder"
