1679defac2a63664c6ba85b57fe3373c78ff00bf
DannyWatson23 <DannyWatson23@outlook.com>
2016-03-03 16:10:55 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo apt-get install -y nodejs
 echo "creating npm installer"
 sudo npm install -g yo generator-hubot
 echo "going into sudo su root"
-sudo su root
+sudo su
 echo "going into shared folder"
 cd /Desktop/Shared
 echo "creating hubot from shared folder"
