fca3cea6f3b44b1b397292abc8d5521c90fa6ab4
DannyWatson23 <DannyWatson23@outlook.com>
2016-03-03 00:26:20 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
-curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash - 
 sudo apt-get install -y nodejs
-sudo npm install -g yo generator-hubot
+sudo mkdir /Desktop/hubot && cd /Desktop/hubot
+sudo npm install -g yo generator-hubot >> /Desktop/hubot
 sudo yo hubot
-sudo nano hubot
