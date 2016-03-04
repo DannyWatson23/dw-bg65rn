7c1d27a50aab60926b5d1b471e27095a7ffbad56
DannyWatson23 <DannyWatson23@outlook.com>
2016-03-04 00:05:12 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+#cd /home/vagrant
+#cd /home/vagrant/bin
 #sudo su
 echo "installing curl"
 curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash - > /dev/null
@@ -11,9 +13,7 @@ sudo chown -R `whoami` ~/.npm
 sudo npm install -g yo > /dev/null
 chmod g+rwx /root /root/.config /root/.config/configstore
 echo "going into shared folder"
-cd /home/vagrant
 yo hubot --defaults > /dev/null
-cd /home/vagrant/bin
 echo 'export HUBOT_CAMPFIRE_TOKEN="a99f3048fb245a8e639ed414e99ce9996aa11c04"' >> hubot
 echo 'export HUBOT_CAMPFIRE_ROOMS="620552"' >> hubot
 echo 'export HUBOT_CAMPFIRE_ACCOUNT="university161"' >> hubot
