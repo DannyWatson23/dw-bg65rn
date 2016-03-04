a3ea7ff73410be426b6ab3552bc6845ee949a1eb
DannyWatson23 <DannyWatson23@outlook.com>
2016-03-04 00:24:14 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,10 +7,10 @@ curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash - > /dev/null
 echo "installing node js"
 sudo apt-get install -y nodejs > /dev/null
 echo "creating npm installer"
-sudo npm install -g hubot coffee-script
-sudo npm install -g yo generator-hubot
-sudo chown -R `whoami` ~/.npm
-sudo npm install -g yo > /dev/null
+npm install -g hubot coffee-script
+npm install -g yo generator-hubot
+chown -R `whoami` ~/.npm
+npm install -g yo > /dev/null
 chmod g+rwx /root /root/.config /root/.config/configstore
 echo "going into shared folder"
 yo hubot --defaults > /dev/null
