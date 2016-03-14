6d128ab2d9f1425f904f37298c321a527b169e21
DannyWatson23 <DannyWatson23@outlook.com>
2016-03-14 15:48:31 +0000

    Update installme.sh

--- installme.sh
+++ installme.sh
@@ -2,18 +2,49 @@
  #cd /home/vagrant
  #cd /home/vagrant/bin
  #sudo su
- echo "installing curl"
- curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash - > /dev/null
- echo "installing node js"
- sudo apt-get install -y nodejs > /dev/null
- echo "creating npm installer"
- npm install -g hubot coffee-script
- npm install -g yo generator-hubot
+
+
+#installing Curl
+if [ -f /etc/apt/sources.list.d/nodesource.list ] ; then echo "Curl has already been installed onto this machine, continuing onto the next program" ; 
+clear
+else echo "This machine does not have Curl installed, it will be installed now";
+curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash - > /dev/null; 
+echo "Curl installed."
+clear
+fi
+
+
+#installing Node Js
+if [ -f /usr/bin/nodejs ] ; then echo "Node JS has already been installed onto this machine, continuing onto the next program"; clear;
+else echo "This machine does not have Node JS installed, it will be installed now";
+sudo apt-get install -y nodejs > /dev/null;
+echo "Node JS installed."
+clear;
+fi
+ 
+ 
+ 
+#Installing NPM
+if [ -f /usr/bin/coffee ]; then echo "NPM has already been installed onto this machine, continuing onto the next program"; clear;
+else echo "This machine does not have NPM installed, it will be installed now";
+npm install -g hubot coffee-script > /dev/null
+npm install -g yo generator-hubot > /dev/null
+echo "NPM installed"
+clear
+fi
+ 
+ 
  chown -R `whoami` ~/.npm
  npm install -g yo > /dev/null
  chmod g+rwx /root /root/.config /root/.config/configstore
   echo "going into shared folder"
-  yo hubot --defaults > /dev/null
+
+if [ -f /home/vagrant/bin/hubot.cmd ]; then echo "Hubot has already been installed onto this machine, continuing onto the rest of the script"; clear;
+else echo "This machine does not have Hubot installed, it will be installed now";
+yo hubot --defaults
+fi
+
+
   cd bin
  echo '#!/bin/sh
  set -e 
